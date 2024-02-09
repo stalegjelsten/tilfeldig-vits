cf6c59b9dc1cb4f6e27360dbe3b4608082b0d58f
Ståle Gjelsten <stalegjelsten@gmail.com>
2024-02-09 20:39:01 +0100

    nytt forsøk på fikse asynkron funksjon

--- script.js
+++ script.js
@@ -2,33 +2,34 @@ let showAnswer = false;
 let showAnswerButton = false;
 let svar = "";
 
-let jokes = fetch('https://stalegjelsten.github.io/tilfeldig-vits/jokes.json').then((response) => response.json()).then((data) => data)
+let jokes = []
+fetch('https://stalegjelsten.github.io/tilfeldig-vits/jokes.json').then((response) => response.json()).then((data) => (jokes = data))
 
 const randJoke = () => {
-  document.getElementById("svar").innerHTML = "";
-  showAnswer = false;
-  const rand = Math.floor(Math.random() * jokes.length);
-  const joke = jokes[rand];
-  svar = joke.svar;
-  let blockquote = document.getElementById("joke");
-  blockquote.innerHTML = joke.vits;
-  if (joke.svar?.length > 0) {
-    document.getElementById("answerBtn").style.display = "inline-block";
-  } else {
-    document.getElementById("answerBtn").style.display = "none";
-  }
-  document.getElementById("answerBtn").innerHTML = "Vis svar";
+    document.getElementById("svar").innerHTML = "";
+    showAnswer = false;
+    const rand = Math.floor(Math.random() * jokes.length);
+    const joke = jokes[rand];
+    svar = joke.svar;
+    let blockquote = document.getElementById("joke");
+    blockquote.innerHTML = joke.vits;
+    if (joke.svar?.length > 0) {
+        document.getElementById("answerBtn").style.display = "inline-block";
+    } else {
+        document.getElementById("answerBtn").style.display = "none";
+    }
+    document.getElementById("answerBtn").innerHTML = "Vis svar";
 };
 
 const visSvar = () => {
-  showAnswer = !showAnswer;
-  if (showAnswer) {
-    document.getElementById("svar").innerHTML = svar;
-    document.getElementById("answerBtn").innerHTML = "Skjul svar";
-  } else {
-    document.getElementById("svar").innerHTML = "";
-    document.getElementById("answerBtn").innerHTML = "Vis svar";
-  }
+    showAnswer = !showAnswer;
+    if (showAnswer) {
+        document.getElementById("svar").innerHTML = svar;
+        document.getElementById("answerBtn").innerHTML = "Skjul svar";
+    } else {
+        document.getElementById("svar").innerHTML = "";
+        document.getElementById("answerBtn").innerHTML = "Vis svar";
+    }
 };
 
 randJoke();
